9569ea85e9c8a81a70b120e3a74cf6f8b7a0f50e
martinPandarski <marto65481@gmail.com>
2021-03-19 09:16:49 +0200

    test2

---
+++ src/components/Catalog/Catalog.js
@@ -0,0 +1,22 @@
+
+// import style from './Catalog.module.css'
+// import CatalogCards from './CatalogCards/CatalogCards';
+
+// const Catalog = ({furniture}) => {
+//     return (
+//       <div className={style.catalogCardsList}>
+//        {furniture.map(item => 
+//           <CatalogCards
+//             key={item.objectId}
+//             description={item.description}
+//             name={item.name}
+//             pictureLink={item.pictureLink}
+//             pricePerOne={item.pricePerOne}
+//             />
+//        )}
+            
+//       </div>
+//     )
+// };
+
+// export default Catalog;
